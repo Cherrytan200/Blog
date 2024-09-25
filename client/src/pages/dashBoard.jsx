0028be3a519e5a0d3df18cba352ce3d822b212c0
charan <cherrytan2003@gmail.com>
2024-09-25 16:04:53 +0530

    complete user image upload functionality

--- client/src/pages/dashBoard.jsx
+++ client/src/pages/dashBoard.jsx
@@ -5,16 +5,16 @@ import DashProfile from '../components/DashProfile.jsx'
 
 export default function DashBoard() {
   const location=useLocation();
-  console.log("location :" ,location.search);
+  
   const [tab,setTab]=useState('');
   useEffect(()=>{
     const urlParmas=new URLSearchParams(location.search);
-    console.log("urlparams",urlParmas);
+    
     const tabFromUrl=urlParmas.get('tab');
     if(tabFromUrl){
       setTab(tabFromUrl);
     }
-    console.log(tabFromUrl);
+    
   },[location.search])
   return (
     <div className='min-h-screen flex flex-col sm:flex-row'>
