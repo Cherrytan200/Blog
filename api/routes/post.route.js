e291c822124e6d7d4ecc532857f59dea84f0bc54
charan <cherrytan2003@gmail.com>
2024-10-05 22:34:44 +0530

    add recent article section to the post page

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -6,6 +6,7 @@ import { create, deletepost, getposts, updatepost } from '../controllers/post.co
 const router=express.Router();
 router.post('/create',verifyToken,create)
 router.get('/getposts',getposts)
+
 router.delete('/deletepost/:postId/:userId',verifyToken,deletepost);
 router.put(`/updatepost/:postId/:userId`,verifyToken,updatepost)
 
